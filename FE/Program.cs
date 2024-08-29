039fa47fdf71185d433ac49716542c883af77ff7
RobbeVanHemelryck <vanhemelryckrobbe@gmail.com>
2024-08-29 21:39:11 +0200

    Working version of .NET 8 client blazor app using MudBlazor

--- FE/Program.cs
+++ FE/Program.cs
@@ -1,23 +1,46 @@
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using FE;
+using FE.Components;
 using Grpc.Net.Client;
-using Grpc.Net.Client.Web;
+using MudBlazor.Services;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
 
-builder.Services.AddSingleton((_) => 
+builder.Services.AddMudServices();
+
+builder.Services.AddSingleton(_ => 
 {
-    var httpClient = new HttpClient(new GrpcWebHandler(GrpcWebMode.GrpcWeb, new HttpClientHandler()));
-    return GrpcChannel.ForAddress("https://localhost:7031", new GrpcChannelOptions { HttpClient = httpClient }); 
+    var httpHandler = new HttpClientHandler
+    {
+        ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+    };
+    // var httpClient = new HttpClient(new GrpcWebHandler(GrpcWebMode.GrpcWeb, new HttpClientHandler()));
+    return GrpcChannel.ForAddress("https://localhost:7031", new GrpcChannelOptions { HttpHandler = httpHandler }); 
 });
 
 builder.Services.AddSingleton(services => new Diaries.Diaries.DiariesClient(services.GetRequiredService<GrpcChannel>()));
 
 builder.Services.AddTransient<console>();
 
-await builder.Build().RunAsync();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
